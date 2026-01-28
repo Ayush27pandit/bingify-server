7fad27bce105af7c43ca6b62b437d2b606185053
Ayush27pandit <ayush12rps@gmail.com>
2026-01-28 23:35:09 +0530

    bug:firebase key

--- src/firebase-auth/firebaseAuth.ts
+++ src/firebase-auth/firebaseAuth.ts
@@ -31,7 +31,10 @@ if (privateKey) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID!,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: formattedKey,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+        : undefined
+
     }),
   });
 } else {
